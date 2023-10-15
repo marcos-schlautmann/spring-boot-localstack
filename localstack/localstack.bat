e31119ca6c34280dfcdf1492e2112a35d67f8e14
marcos-schlautmann <marcos27.bonfim@gmail.com>
2023-10-15 20:45:17 -0300

    Adicionando dependencias, configurações de projeto, criando configuration e controller, criando algumas chaves e o bucket helloworld

--- localstack/localstack.bat
+++ localstack/localstack.bat
@@ -1,2 +1,9 @@
 @echo off
 aws --endpoint http://localhost:4566 --profile default ssm put-parameter --name "/config/spring-boot-localstack_default/helloWorld" --value "Hello World Parameter Store" --type String
+
+aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/spring-boot-localstack_default --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
+aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/spring-boot-localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
+aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/application --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
+aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/application_localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
+
+aws --endpoint http://localhost:4566 --profile default s3 mb s3://s3-helloworld
