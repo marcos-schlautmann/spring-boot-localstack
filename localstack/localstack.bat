5e4b57b9314d9ee37ff064f572294837aeb6d351
marcos-schlautmann <marcos27.bonfim@gmail.com>
2023-10-15 21:38:30 -0300

    criando, controller, dto, service, add dependencias.

--- localstack/localstack.bat
+++ localstack/localstack.bat
@@ -4,6 +4,6 @@ aws --endpoint http://localhost:4566 --profile default ssm put-parameter --name 
 aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/spring-boot-localstack_default --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
 aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/spring-boot-localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
 aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/application --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
-aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/application_localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
+aws --endpoint http://localhost:4566 --profile default secretsmanager create-secret --name /secret/application_default --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
 
 aws --endpoint http://localhost:4566 --profile default s3 mb s3://s3-helloworld
